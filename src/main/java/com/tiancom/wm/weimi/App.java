5f85aedac29b16e205d356f65444f9f0101c6ed2
nongfei <nongfei@gmail.com>
2017-09-18 17:15:23 +0800

    weimi
    weimi

---
+++ src/main/java/com/tiancom/wm/weimi/App.java
@@ -0,0 +1,13 @@
+package com.tiancom.wm.weimi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
